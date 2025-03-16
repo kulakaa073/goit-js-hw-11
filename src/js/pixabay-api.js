5c02809f4c7a8dfbc14afbbb13aa1c11b2b107a0
Shade <kulakaa073@gmail.com>
2025-03-16 16:42:31 +0200

    Refactoring
    May need to move code to inside the function in modules

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-axios.defaults.headers.common["key"] = '49331569-d75c2214014d7ff2c794993e6';
 const params = {
-    q: '',
+    key: '49331569-d75c2214014d7ff2c794993e6',
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
 }
 
-function searhRequest(searchData) {
+export default function searchRequest(searchData) {
     params.q = searchData;
     return axios.get('', { params });
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-//import fetchData from './js/pixabay-api';
-//import renderData from './js/render-functions';
+import fetchData from './js/pixabay-api';
+import renderData from './js/render-functions';
 
-import axios from "axios";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const params = {
-    key: '49331569-d75c2214014d7ff2c794993e6',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
+const searchInput = document.querySelector('input[name="search-text"]');
+const searchButton = document.querySelector('button[type="submit"]');
+const searchForm = document.querySelector('.form');
+
+let loadingMessage = document.querySelector('.loading');
+if (!loadingMessage) {
+    searchForm.insertAdjacentHTML('afterend', '<p class="loading visually-hidden">Loading images, please wait...<span class="loader"></span></p>');
+    loadingMessage = document.querySelector('.loading');
 }
 
 const iziToastNoInputSettings = {
@@ -41,67 +40,15 @@ const iziToastNoImageSettings = {
     progressBarColor: '#B51B1B'
 }
 
-function searchRequest(searchData) {
-    params.q = searchData;
-    return axios.get('', { params });
-}
-
-function renderData(data) {
-    gallery.innerHTML = '';
-    const galleryCollectionTemplate = [];
-    data.forEach(item => {
-        galleryCollectionTemplate.push(
-            `<li>
-                <a class="gallery-link" href="${item.largeImageURL}">
-                    <img src="${item.previewURL}" alt="${item.tags}">
-                    <ul>
-                        <li>
-                            <p>Likes: <span>${item.likes}</span></p>
-                        </li>
-                        <li>
-                            <p>Views: <span>${item.views}</span></p>
-                        </li>
-                        <li>
-                            <p>Comments: <span>${item.comments}</span></p>
-                        </li>
-                        <li>
-                            <p>Downloads: <span>${item.downloads}</span></p>
-                        </li>
-                    </ul>
-                </a>
-            </li>`
-        );
-    });
-    gallery.insertAdjacentHTML('afterbegin', galleryCollectionTemplate.join(''));
-    lightboxGallery.refresh();
-}
-
-const searchInput = document.querySelector('input[name="search-text"]');
-const searchButton = document.querySelector('button[type="submit"]');
-const searchForm = document.querySelector('.form');
-let gallery = document.querySelector('.gallery');
-if (!gallery) {
-    searchForm.insertAdjacentHTML('afterend', '<ul class="gallery"></ul>');
-    gallery = document.querySelector('.gallery');
-}
-let loadingMessage = document.querySelector('.loading');
-if (!loadingMessage) {
-    searchForm.insertAdjacentHTML('afterend', '<p class="loading visually-hidden">Loading images, please wait...<span class="loader"></span></p>');
-    loadingMessage = document.querySelector('.loading');
-}
-
-//const galleryElementTemplate = '<ul class="gallery"></ul>';
-
 searchButton.addEventListener('click', (e) => {
     e.preventDefault();
     const searchData = searchInput.value;
-    console.log("searchdata: " + searchData);
     if (searchData.trim() === '') {
         iziToast.show({ ...iziToastNoInputSettings });
         return;
     }
-    loadingMessage.classList.remove('visually-hidden');
-    searchRequest(searchData)
+    loadingMessage.classList.remove('visually-hidden'); //deleting and adding element might be better? rethink
+    fetchData(searchData)
         .then((response) => {
             loadingMessage.classList.add('visually-hidden');
             if (response.data.totalHits > 0) {
@@ -116,10 +63,3 @@ searchButton.addEventListener('click', (e) => {
             console.error('Error:', error);
         });
 });
-
-const lightboxOptions = {
-    captionsData: 'alt',
-    captionDelay: 250
-}
-
-const lightboxGallery = new SimpleLightbox('.gallery a', { ...lightboxOptions });
